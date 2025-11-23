7a37584ed34fb78e870c9307efc2d65b943733ee
sonervarkan <sonervarkan1455@yahoo.com>
2025-11-23 11:21:14 +0300

    Update database.js

--- utility/database.js
+++ utility/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
-    database: 'webprogramlamaeticaret'
+    password: '****',
+    database: '****'
 });
 
 conn.connect((err) => {
